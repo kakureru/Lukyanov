f9f682079e3b1774b3f6fc259063fb31e1dc3cfd
Lukyanov Svyatoslav <svyat.lknv@yandex.ru>
2023-02-03 23:07:29 +0300

    Setup nav graph

--- app/src/main/java/com/example/lukyanovtinkoff/app/presentation/PopularFragment.kt
+++ app/src/main/java/com/example/lukyanovtinkoff/app/presentation/PopularFragment.kt
@@ -1,4 +1,27 @@
 package com.example.lukyanovtinkoff.app.presentation
 
-class PopularFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lukyanovtinkoff.databinding.FragmentPopularBinding
+
+class PopularFragment : Fragment() {
+
+    private var _binding: FragmentPopularBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPopularBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
